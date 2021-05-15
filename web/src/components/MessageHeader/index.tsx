1838caf58d5e264d30d0790517088d92c66770c7
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-05-15 22:49:14 +0800

    message header ui changes

--- web/src/components/MessageHeader/index.tsx
+++ web/src/components/MessageHeader/index.tsx
@@ -6,16 +6,10 @@ export default function MessageHeader() {
   return (
     <div className="message-header">
       <div className="message-header-left">
-        <img
-          className="square-icon"
-          src="https://expertphotography.com/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"
-          alt=""
-        />
         <div className="message-header-details-container">
           <h1>@ryantillaman</h1>
           <div className="status">
             <span className="online-circle"></span>
-            <p>Online</p>
           </div>
         </div>
       </div>
