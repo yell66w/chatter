a0508f8155dabce5e2c3ba4c69ee30f8cfad6ea4
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-05-15 23:03:19 +0800

    fixed vertical scrolling margin top first child

--- web/src/components/MessageContent/index.tsx
+++ web/src/components/MessageContent/index.tsx
@@ -27,11 +27,229 @@ export default function MessageContent() {
         "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       createdAt: "12:09 PM",
     },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "sd There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
   ];
   return (
     <div className="message-content-container">
       {messageArray.map((item, index) => {
-        return <MessageBubble key={item._id} data={item} />;
+        return <MessageBubble key={item._id + index} data={item} />;
       })}
     </div>
   );
