b6ffa0974f596624a53f617845f32175f0a76c44
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-05-15 22:40:14 +0800

    bubbles array

--- web/src/components/MessageBubble/index.tsx
+++ web/src/components/MessageBubble/index.tsx
@@ -1,41 +1,27 @@
 import React from "react";
 import "./styles.scss";
-const MessageBubble = () => {
+interface Props {
+  data: {
+    _id: string;
+    name: string;
+    content: string;
+    image: string;
+    createdAt: string;
+  };
+}
+const MessageBubble = ({ data }: Props) => {
   return (
     <div className="message-bubble">
       <div className="image-container">
-        <img
-          className="square-icon-small"
-          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          alt="profile"
-        />
+        <img className="square-icon-small" src={data.image} alt="profile" />
       </div>
 
       <div className="bubble-container">
         <h1>
-          Ryan Tillaman <span className="bubble-date">11:58 AM</span>
+          {data.name} <span className="bubble-date">{data.createdAt}</span>
         </h1>
         <div className="bubble-text-container">
-          <div className="bubble-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            aliquid nam soluta? Excepturi at, nihil soluta nobis, neque, laborum
-            animi iure sint magnam eos amet quis. Possimus cumque praesentium
-            nostrum. Alias rem blanditiis, voluptatem exercitationem qui dolorem
-            fuga facilis distinctio temporibus deserunt iure repellendus
-            laboriosam reprehenderit impedit omnis, neque numquam fugiat aperiam
-            illo veniam dolorum voluptates nobis? Ullam, quisquam atque. Lorem
-            ipsum dolor sit amet consectetur, adipisicing elit. Ipsum hic
-            accusantium deserunt accusamus saepe nemo aut iure aliquam. Fuga
-            quas eveniet in dicta aspernatur. Obcaecati magnam ipsam amet
-            laboriosam sint? Et est enim consequuntur iusto cum voluptate hic
-            ipsum id debitis repellendus voluptates nesciunt odit obcaecati
-            alias distinctio commodi similique, harum esse maxime nulla fugiat
-            accusamus. Cupiditate porro sapiente quis? Molestias aliquid et
-            repudiandae doloribus, non saepe ea quia asperiores? Minus, quidem
-            vitae excepturi totam, voluptate sequi, temporibus eligendi velit
-            praesentium est corporis eum. Atque odit velit voluptates
-            consequuntur eligendi.
-          </div>
+          <div className="bubble-text">{data.content}</div>
         </div>
       </div>
     </div>
--- web/src/components/MessageContent/index.tsx
+++ web/src/components/MessageContent/index.tsx
@@ -2,26 +2,37 @@ import MessageBubble from "@components/MessageBubble";
 import React from "react";
 import "./styles.scss";
 export default function MessageContent() {
+  const messageArray = [
+    {
+      _id: "01",
+      name: "Ryan Tillaman",
+      content: "First There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "11:58 AM",
+    },
+    {
+      _id: "02",
+      name: "Rosario Tillaman",
+      content: "Second the motion There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:00 PM",
+    },
+    {
+      _id: "03",
+      name: "Nathalia Tillaman",
+      content: "Third There",
+      image:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      createdAt: "12:09 PM",
+    },
+  ];
   return (
     <div className="message-content-container">
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
-      <MessageBubble />
+      {messageArray.map((item, index) => {
+        return <MessageBubble key={item._id} data={item} />;
+      })}
     </div>
   );
-  <MessageBubble />;
 }
