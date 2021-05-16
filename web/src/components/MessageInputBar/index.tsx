0bf91f47646fc3ece25d93b8f6fdaafb5d740811
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-05-16 17:45:40 +0800

    fonts changes

--- web/src/components/MessageInputBar/index.tsx
+++ web/src/components/MessageInputBar/index.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { ChangeEvent, KeyboardEvent } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { IoAddCircle, IoImage } from "react-icons/io5";
+import { IoMdImage } from "react-icons/io";
 import "./styles.scss";
+import TextareaAutosize from "react-textarea-autosize";
 export default function MessageInputBar() {
-  return <div className="message-input-container"></div>;
+  return (
+    <div className="message-input-container">
+      <div className="message-input-box">
+        <div className="message-tabs">
+          <IoAddCircle size={25} />
+          <IoMdImage size={23} />
+        </div>
+        <div className="message-textarea">
+          <TextareaAutosize
+            placeholder="Message @ryantillaman"
+            className="message-input"
+            maxRows={6}
+          />
+          {/* <textarea
+            placeholder="Message @ryantillaman"
+            className="message-input"
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
 }
